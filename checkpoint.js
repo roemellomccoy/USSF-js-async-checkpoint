c47c137581f0ce1240746b6ae496bcfa0a4b079f
roemellomccoy <83963597+roemellomccoy@users.noreply.github.com>
2021-05-18 14:24:58 -0500

    Add files via upload

--- checkpoint.js
+++ checkpoint.js
@@ -6,6 +6,7 @@ var typeArray = [];
 function getTypes(fileName){
   let pokemonInput = fs.readFileSync(fileName, 'utf-8');
   let pokemonArray = pokemonInput.split('\r\n');
+  console.log(pokemonArray);
 
   for(let i = 0; i < pokemonArray.length; i++){
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonArray[i]}`)
@@ -22,5 +23,25 @@ function getTypes(fileName){
 
   }
 }
+
+// function getTypes(fileName) {
+//   let pokemonArray = fs.readFileSync(fileName, `utf-8`)
+//   pokemonArray = pokemonArray.split(`\n`);
+//   console.log(pokemonArray);
+
+//   for (let i = 0; i < pokemonArray.length; i++) {
+//     let typeArray = [];
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonArray[i]}`)
+//       .then(res => res.json())
+//       .then(res => {
+//         for (let j = 0; j < res.types.length; j++) {
+//           typeArray.push(res.types[j].type.name)
+//         }
+//         let resultStr = `${pokemonArray[i]}: ${typeArray.join(`, `)}`
+//         console.log(resultStr)
+//       })
+//     }
+//   }
+
 
 getTypes("C:/Users/roeme/OneDrive/Documents/Cohort/USSF-js-async-checkpoint/input.txt");
